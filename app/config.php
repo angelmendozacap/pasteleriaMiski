2682a6589c9beeac5d59b8cf0d8edb2a2f10febf
Alfredo Mendoza <kick-1930@hotmail.com>
2019-03-15 16:19:34 -0500

    Se configuro el archivo de configuración

--- app/config.php
+++ app/config.php
@@ -1,10 +1,10 @@
 <?php
 
 define('DB', [
-    'host' => 'localhost',
+    'host' => 'zj2x67aktl2o6q2n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     'port' => '3306',
-    'user' => 'root',
-    'password' => '',
-    'database' => 'miski',
+    'user' => 'mot62c4hibm51yz4',
+    'password' => 'nphwibshhq9t76bf',
+    'database' => 'n9j8kutdkhqenx49',
     'charset' => 'utf8'
 ]);
